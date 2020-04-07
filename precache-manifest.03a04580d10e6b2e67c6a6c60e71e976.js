ad482a780ccb37366462c30c71e9c01aba49d807
Sahir <sahirmpatel@gmail.com>
2020-04-07 15:48:08 +0530

    Updates

---
+++ precache-manifest.03a04580d10e6b2e67c6a6c60e71e976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6244ab78f60cf2b729136b09fa7e728",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "4080a8c90beaf8c3f669",
+    "url": "/react-recipe-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "613be47642bece89a976",
+    "url": "/react-recipe-app/static/css/main.298dd102.chunk.css"
+  },
+  {
+    "revision": "4080a8c90beaf8c3f669",
+    "url": "/react-recipe-app/static/js/2.afddff32.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-recipe-app/static/js/2.afddff32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613be47642bece89a976",
+    "url": "/react-recipe-app/static/js/main.1b8bc62b.chunk.js"
+  },
+  {
+    "revision": "d8c2394243e212a300fd",
+    "url": "/react-recipe-app/static/js/runtime-main.0af5c580.js"
+  }
+]);
